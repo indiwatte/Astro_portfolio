28bf742fbe394d3bf53c59fec9939e223f514544
Watte <Indi.watte@student.howest.be>
2025-11-01 15:47:31 +0100

    making the config.ts file

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,8 @@ const projects = defineCollection({
     type: "content",
     schema: z.object({
         title: z.string(),
-        slug: z.string(),
+        description: z.string().optional(),
+        date: z.string().optional(),
     }),
 });
 
