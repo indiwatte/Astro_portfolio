4a7d893327ff694b8e971db86909f3574e9ae387
Watte <Indi.watte@student.howest.be>
2025-11-09 12:29:32 +0100

    config update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,30 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-
 const projects = defineCollection({
-    loader: glob({pattern: "**/*.md",base: "src/content/projects",}),
+    loader: glob({
+        pattern: "**/*.md",
+        base: "src/content/projects",
+    }),
     schema: z.object({
         title: z.string(),
         description: z.string().optional(),
         date: z.string().optional(),
+        image: z.any().optional(), 
     }),
 });
 
-
-const blogs = defineCollection({
-    loader: glob({pattern: "**/*.md",base: "src/content/blogs",}),
+const journals = defineCollection({
+    loader: glob({
+        pattern: "**/*.md",
+        base: "src/content/journals",
+    }),
     schema: z.object({
         title: z.string(),
         description: z.string().optional(),
         date: z.string().optional(),
+        image: z.any().optional(), 
     }),
 });
 
-export const collections = { projects, blogs };
+export const collections = { projects, journals };
